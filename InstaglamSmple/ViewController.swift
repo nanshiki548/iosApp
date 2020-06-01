c3ca6df2f8f29bd4ae8501e2c5a26d4e38721b88
MatsudaTatsuya <matudatatuya7@gmail.com>
2020-06-01 18:04:07 +0900

    Initial Commit

---
+++ InstaglamSmple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaglamSmple
+//
+//  Created by 松田竜弥 on 2020/06/01.
+//  Copyright © 2020 松田竜弥. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
